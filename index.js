d2d1171a11104bac0c44c3925b0c2baaec3b4540
nathalia-uf <112099626+nathalia-uf@users.noreply.github.com>
2023-03-29 15:33:06 -0400

    change 2

--- index.js
+++ index.js
@@ -16,41 +16,53 @@ form.addEventListener("submit", async (event) => {
         const data = await response.json();
         console.log(data);
         weatherDiv.innerHTML = "";
-        var h2 = document.createElement('h2');
+        const h2 = document.createElement('h2');
         h2.textContent = searchTerm.toUpperCase();
         weatherDiv.appendChild(h2);
 
-        var mapLink = document.createElement('a');
+        const mapLink = document.createElement('a');
         mapLink.href = `https://www.google.com/maps/search/?api=1&query=${data.coord.lat},${data.coord.lon}`;
         mapLink.target = "_blank";
         mapLink.textContent = `View on Map`;
         weatherDiv.appendChild(mapLink);
 
-        var iconCode = data.weather[0].icon;
-        var description = data.weather[0].description;
+        const iconCode = data.weather[0].icon;
+        const description = data.weather[0].description;
 
-        var icon = document.createElement('img');
+        const icon = document.createElement('img');
         icon.src = `https://openweathermap.org/img/wn/${iconCode}.png`;
         icon.alt = description;
         weatherDiv.appendChild(icon);
 
-        var condition = document.createElement('p');
+        const condition = document.createElement('p');
         condition.textContent = description;
         weatherDiv.appendChild(condition);
 
-        var currentTemp = data.main.temp;
-        var temp = document.createElement('p');
+        const currentTemp = data.main.temp;
+        const temp = document.createElement('p');
         temp.textContent = `Current: ${Math.round(currentTemp)}°F`;
         weatherDiv.appendChild(temp);
 
-        var feelsLikeTemp = data.main.feels_like;
-        var feelsLike = document.createElement('p');
+        const feelsLikeTemp = data.main.feels_like;
+        const feelsLike = document.createElement('p');
         feelsLike.textContent = `Feels like: ${Math.round(feelsLikeTemp)}°F`;
         weatherDiv.appendChild(feelsLike);
 
-        var dt = data.dt;
-        var updated = document.createElement('p');
+        const dt = data.dt;
+        const updated = document.createElement('p');
         updated.textContent = `Last updated: ${new Date(dt * 1000).toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' }).toLowerCase()}`;
         weatherDiv.appendChild(updated);
-    })
-};
+    }else {
+        weatherDiv.innerHTML = "<p>Location not found</p>";
+      }
+    } catch (error) {
+      console.error(error);
+      weatherDiv.innerHTML = "<p>Something went wrong</p>";
+    }
+  });
+
+  function getLocalTime(sec) {
+    return new Date(sec * 1000)
+      .toLocaleTimeString("en-US", { hour: "numeric", minute: "numeric" })
+      .toLowerCase();
+  }
